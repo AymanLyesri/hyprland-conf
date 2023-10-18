e6a7582638331d5722c942aceb5a817dcbc7dda6
AymanLyesri <lyesri99@gmail.com>
2023-10-18 20:12:51 +0100

    + custom styles
    Former-commit-id: c8a8f6e762a44281301ae854827cab2dc41f7f78
    Former-commit-id: 2f134d2ea2e134759153d919dbebbb95be778209

--- eww/scripts/quotes.py
+++ eww/scripts/quotes.py
@@ -8,12 +8,11 @@ KEY=os.getenv("KEY")
 
 from textwrap import TextWrapper
 
-text = "Given text and a desired line length, wrap the text as a typewriter would. Insert a newline character  '\\n' after each word that reaches or exceeds the desired line length."
-
 tw = TextWrapper()
 tw.width = int(sys.argv[1])
 
 category = 'happiness'
+
 api_url = 'https://api.api-ninjas.com/v1/quotes?category={}&limit=1'.format(category)
 response = requests.get(api_url, headers={'X-Api-Key': KEY})
 
