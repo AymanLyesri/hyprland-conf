3454d96f93ee36f48a9fd844854cc5489bc5640b
Ayman Lyesri <lyesri99@gmail.com>
2023-11-05 19:17:09 +0100

    new wallpapers*

--- scripts/hyprpaper/load_config.sh
+++ scripts/hyprpaper/load_config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-random=$(find /home/ayman/.config/hypr/wallpapers -type f | shuf -n 1)
+# random=$(find /home/ayman/.config/hypr/wallpapers -type f | shuf -n 1)
 
-PATH=/home/ayman/.config/hypr/hyprpaper.conf
+# PATH=/home/ayman/.config/hypr/hyprpaper.conf
 
-> $PATH
+# > $PATH
 
-echo "preload = $random" >> $PATH
+# echo "preload = $random" >> $PATH
 
-echo "wallpaper = eDP-1,$random" >> $PATH
+# echo "wallpaper = eDP-1,$random" >> $PATH
