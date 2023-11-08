085b68e32fe59a7d8ac75811f21269b646cb60da
Ayman Lyesri <lyesri99@gmail.com>
2023-11-08 20:19:33 +0100

    minor tweaks to pywal
    Former-commit-id: 52cb0b69a92a1ed1812ecbf0d7da823e7516ffb9
    Former-commit-id: b5037a51975918b0833f88c6ca7d3f8b4ff6fe48

--- hyprpaper/load.sh
+++ hyprpaper/load.sh
@@ -2,6 +2,7 @@
 
 random=$(find /home/ayman/.config/hypr/wallpapers/normal -type f | shuf -n 1)
 config=/home/ayman/.config/hypr/hyprpaper.conf
+monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
 > $config
 
@@ -11,6 +12,9 @@ do
 done
 
 echo "" >> $config
-echo "wallpaper = eDP-1,$random" >> $config
+echo "wallpaper = $monitor,$random" >> $config
+
+#################################################
 
 hyprpaper
+source /home/ayman/.config/hypr/wal/pywal $random
--- hyprpaper/random.sh
+++ hyprpaper/random.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 random=$(find /home/ayman/.config/hypr/wallpapers/normal -type f | shuf -n 1)
 
-wal -i $random &
-
 monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+
+##########################################
+
+source /home/ayman/.config/hypr/wal/pywal $random
 
 hyprctl hyprpaper wallpaper "$monitor,$random"
 
