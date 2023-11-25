741975e70d6feb8512498d757569aa2052f4caee
Ayman Lyesri <lyesri99@gmail.com>
2023-11-25 19:38:14 +0100

    Remove unused files and update neovim configuration
    Former-commit-id: 16c40ce33eef604f040603512a7f9cfeb5c484c5
    Former-commit-id: 378b9960a17032c0688fe1fec9779d351b5c45d0

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,38 @@
-vim.opt.relativenumber = true
-vim.g.neovide_cursor_animation_length = 0.069
-vim.g.neovide_cursor_trail_size = 0.69
+package.path = "/home/ayman/.config/hypr/nvim/plugins.lua"
+require('plugins')
 
+-- local lfs = require('lfs')
+
+-- -- Directory containing the Lua files
+-- local dir = "/home/ayman/.config/hypr/nvim/"
+
+-- -- Iterate over all files in the directory
+-- for file in lfs.dir(dir) do
+--     -- If the file is a Lua filelua-filesystem
+--     if file:match(".lua$") then
+--         -- Add the directory and file to the package path
+--         package.path = package.path .. ";" .. dir .. file
+--         -- Require the file without the .lua extension
+--         require(file:sub(1, -5))
+--     end
+-- end
+
+
+-- Set line numbers and relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+
+
+-- Set encoding to UTF-8
+vim.opt.encoding = "UTF-8"
+
+-- Neovide settings
+vim.g.neovide_padding_top = 0
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_right = 0
+vim.g.neovide_padding_left = 0
+
+-- vim.g.neovide_transparency = 0.69
+-- vim.g.transparency = 0.69
+-- vim.g.neovide_background_color = '#0f1117' .. string.format('%x', math.floor(255 * vim.g.transparency))
