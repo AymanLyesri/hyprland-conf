f16d7518f27f6864ec221546274e0e06a567f986
Ayman Lyesri <lyesri99@gmail.com>
2023-12-03 16:37:50 +0100

    added wallpaper support for multi-monitor

--- hyprpaper/load.sh
+++ hyprpaper/load.sh
@@ -2,13 +2,17 @@
 
 random=$(find /home/ayman/.config/hypr/wallpapers/normal -type f | shuf -n 1)
 config=/home/ayman/.config/hypr/hyprpaper.conf
-monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
-> $config
+>$config
 
-echo "preload = $random" >> $config
-echo "" >> $config
-echo "wallpaper = $monitor,$random" >> $config
+echo "preload = $random" >>$config
+echo "" >>$config
+
+for monitor in $monitors; do
+    echo "wallpaper = $monitor,$random" >>$config
+done
+
 hyprpaper &
 
 #################################################
@@ -17,8 +21,7 @@ source /home/ayman/.config/hypr/wal/pywal $random
 
 #################################################
 
-for wallpaper in $(find /home/ayman/.config/hypr/wallpapers/normal -type f)
-do
+for wallpaper in $(find /home/ayman/.config/hypr/wallpapers/normal -type f); do
     hyprctl hyprpaper preload $wallpaper
     # echo "preload = $wallpaper" >> $config
 done
