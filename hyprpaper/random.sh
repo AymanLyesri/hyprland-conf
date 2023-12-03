84f9ebb4dd3f37caf7590e02683a9264f257e29a
Ayman Lyesri <lyesri99@gmail.com>
2023-12-03 16:37:50 +0100

    added wallpaper support for multi-monitor
    Former-commit-id: f16d7518f27f6864ec221546274e0e06a567f986
    Former-commit-id: d5f3f4daf004df15acd40037623c51cb43ce8e98

--- hyprpaper/random.sh
+++ hyprpaper/random.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 random=$(find /home/ayman/.config/hypr/wallpapers/normal -type f | shuf -n 1)
 
-monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
+monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
 ##########################################
 
-hyprctl hyprpaper wallpaper "$monitor,$random"
+for monitor in $monitors; do
+    hyprctl hyprpaper wallpaper "$monitor,$random"
+done
 
 source /home/ayman/.config/hypr/wal/pywal $random
 
