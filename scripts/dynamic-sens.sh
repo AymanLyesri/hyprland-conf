d549339fab04cb03c7969133817dc7a3d4cc4eb5
AymanLyesri <lyesri99@gmail.com>
2023-12-06 13:56:27 +0100

    small fixes
    Former-commit-id: 5b83827abe57a0f11f9c0ecb7a78942e72f6add8
    Former-commit-id: d32655f98520e067a6afc7e87ce814eb073f1f41

--- scripts/dynamic-sens.sh
+++ scripts/dynamic-sens.sh
@@ -4,15 +4,11 @@
 chassis_type=$(hostnamectl | grep "Chassis:" | awk '/Chassis/{print $2}')
 sens_file="$HOME/.config/hypr/configs/general.conf"
 
-echo "Chassis type: $chassis_type"
-
 if [ $chassis_type == "laptop" ]; then
-    new_sensitivity="0.3"
-    echo "Laptop detected, setting sensitivity to $new_sensitivity"
+    new_sensitivity="0.4"
 else
     new_sensitivity="0.2"
-    # echo "Desktop detected, setting sensitivity to $new_sensitivity"
 fi
 
 # Use sed to replace the sensitivity value in the file
-sed -i "s/sensitivity=/sensitivity=$new_sensitivity/g" $sens_file
+sed -i "s/sensitivity=*.*/sensitivity=$new_sensitivity/g" $sens_file
