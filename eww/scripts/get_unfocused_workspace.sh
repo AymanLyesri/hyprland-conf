8a25d9ff94e17bcf75e7c47e8657515e9334761a
Ayman Lyesri <lyesri99@gmail.com>
2023-12-11 16:57:28 +0100

    Updated bar and file permissions

---
+++ eww/scripts/get_unfocused_workspace.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+counter=0
+
+socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r line; do
+    sleep 0.5
+    echo $(hyprctl monitors | grep active | awk '{print $3}')
+done
