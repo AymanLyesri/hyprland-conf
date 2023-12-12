9d277f718a5ab2a9081fc5a08cbfe4395dfd2281
Ayman Lyesri <lyesri99@gmail.com>
2023-12-12 22:08:20 +0100

    Update general.conf, monitors.conf, window_rules.conf, bar_center.yuck, bar_right.yuck, bar_center.scss, hyprland.conf, load.sh, random.sh

--- hyprpaper/random.sh
+++ hyprpaper/random.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 random=$(find /home/ayman/.config/hypr/wallpapers/normal -type f | shuf -n 1) # get random wallpaper
 monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')                # get monitors
-
+hyprdir=$HOME/.config/hypr                                                    # hypr directory
 ##########################################
 
-for monitor in $monitors; do
+for monitor in $monitors; do                       # loop through monitors
     hyprctl hyprpaper wallpaper "$monitor,$random" # set wallpaper
 done
 
-source /home/ayman/.config/hypr/wal/pywal $random # set wallpaper theme
+##########################################
+
+source $hyprdir/wal/pywal $random         # set wallpaper theme
+source $hyprdir/scripts/dynamic-border.sh # set border theme
 
 ##########################################
 
-source /home/ayman/.config/hypr/scripts/eww_reset # reset eww
+source $hyprdir/scripts/eww_reset # reset eww
