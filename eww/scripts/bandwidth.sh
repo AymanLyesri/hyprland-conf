700105498218d5c5dc15e0eb3b505d9f55933e66
Ayman Lyesri <lyesri99@gmail.com>
2023-12-13 16:37:37 +0100

    Update bar_center and bandwidth scripts

--- eww/scripts/bandwidth.sh
+++ eww/scripts/bandwidth.sh
@@ -2,18 +2,33 @@
 
 # This script is used to show the bandwidth usage of a given interface
 
-get_up() {
-    bandwidth=$(sudo iftop -t -s 1 -n -N -i any | grep -E 'Total send rate' | awk '{print $4}')
+# Network interface name
+interface="enp8s0" # Change this to your actual interface name
 
-    echo $bandwidth | sed 's/[^0-9]*//g' | awk '{printf "%.2f", $1 / 1024}'
+# Function to extract bytes from ip command output
+get_bytes() {
+    ip -s link show "$interface" | awk '/'"$1"':/{getline; print $1}'
 }
 
-case "$1" in
-"--up")
-    get_up
-    ;;
-"--down")
-    # echo 1
-    ;;
-*) ;;
-esac
+# Get initial values
+rx_old=$(get_bytes "RX")
+tx_old=$(get_bytes "TX")
+sleep 1
+
+while true; do
+    # Get new values
+    rx_new=$(get_bytes "RX")
+    tx_new=$(get_bytes "TX")
+
+    # Calculate speed
+    rx_speed=$((($rx_new - $rx_old) / 1024)) # KB per second with two decimal places
+    tx_speed=$((($tx_new - $tx_old) / 1024)) # KB per second with two decimal places
+
+    # Display results
+    echo "[$tx_speed,$rx_speed]"
+
+    # Update old values
+    rx_old=$rx_new
+    tx_old=$tx_new
+    sleep 1
+done
