c11fd03dd9fa30f65c20b1205cdab2ccd551e9df
Ayman Lyesri <lyesri99@gmail.com>
2023-12-14 15:37:29 +0100

    whole overhaul
    Former-commit-id: d90da40b541320fdfa394f6a2d17d84dd973fc0b
    Former-commit-id: 93a87b4a28001950316432955d28a6682a3bed13

--- hyprpaper/random.sh
+++ hyprpaper/random.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 random=$(find /home/ayman/.config/hypr/wallpapers/normal -type f | shuf -n 1) # get random wallpaper
-monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}')                # get monitors
-hyprdir=$HOME/.config/hypr                                                    # hypr directory
+# random="$(echo '/home/ayman/.config/hypr/wallpapers/normal/yande.re 826221 ass bikini breast_hold feet mitsu_(mitsu_art) natsuki_subaru ram_(re_zero) re_zero_kara_hajimeru_isekai_seikatsu rem_(re_zero) swimsuits topless.jpg')"
+monitors=$(hyprctl monitors | grep Monitor | awk '{print $2}') # get monitors
+hyprdir=$HOME/.config/hypr
+
+echo $random
+
 ##########################################
 
-hyprctl hyprpaper preload $random # preload wallpaper
+source $hyprdir/wal/pywal "$random"         # set wallpaper theme
+source $hyprdir/scripts/dynamic-border.sh & # set border theme
+
+##########################################w
+
+source $hyprdir/scripts/eww_reset & # reset eww
+
+##########################################
+
+hyprctl hyprpaper preload "$random" # preload wallpaper
 
 for monitor in $monitors; do                       # loop through monitors
     hyprctl hyprpaper wallpaper "$monitor,$random" # set wallpaper
 done
 
-hyprctl hyprpaper unload $random # preload wallpaper
-
-##########################################
-
-source $hyprdir/wal/pywal $random         # set wallpaper theme
-source $hyprdir/scripts/dynamic-border.sh # set border theme
-
-##########################################
-
-source $hyprdir/scripts/eww_reset # reset eww
+hyprctl hyprpaper unload all # unload wallpaper
