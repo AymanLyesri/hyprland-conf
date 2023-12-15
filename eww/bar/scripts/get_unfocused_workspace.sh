20ce1dc26ac24702e50d5526ee265cb10e639e00
Ayman Lyesri <lyesri99@gmail.com>
2023-12-15 18:49:16 +0100

    Update eww theme and scripts
    Former-commit-id: b19e25db44d3964a601c68c21ef1ba50793f0b44
    Former-commit-id: bf189e8a397d996f7f62473fe47e157d68cd8b6c

--- eww/bar/scripts/get_unfocused_workspace.sh
+++ eww/bar/scripts/get_unfocused_workspace.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-counter=0
-
 socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r line; do
     sleep 0.5
     echo $(hyprctl monitors | grep active | awk '{print $3}')
