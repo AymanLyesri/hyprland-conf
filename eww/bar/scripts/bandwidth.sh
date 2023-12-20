e3c1ebe02e13972d8987f9358843518d690f4a9d
AymanLyesri <lyesri99@gmail.com>
2023-12-20 13:13:40 +0100

    Update bandwidth.sh and variables.yuck scripts

--- eww/bar/scripts/bandwidth.sh
+++ eww/bar/scripts/bandwidth.sh
@@ -3,10 +3,10 @@
 # This script is used to show the bandwidth usage of a given interface
 
 # Get the network interface associated with Ethernet (enp)
-enp_interface=$(ip -o link show | awk '/^[0-9]+: enp/{print $2}' | cut -d':' -f1)
+enp_interface=$(ip -o link show up | grep 'state UP' | awk '{print $2}' | sed 's/://')
 
 # Network interface name
-interface="enp8s0" # Change this to your actual interface name
+# enp_interface="lo" # Change this to your actual interface name
 
 # Function to extract bytes from ip command output
 get_bytes() {
@@ -22,14 +22,14 @@ while true; do
     # Get new values
     rx_new=$(get_bytes "RX")
     tx_new=$(get_bytes "TX")
-
+    
     # Calculate speed
     rx_speed=$((($rx_new - $rx_old) / 1024)) # KB per second with two decimal places
     tx_speed=$((($tx_new - $tx_old) / 1024)) # KB per second with two decimal places
-
+    
     # Display results
     echo "[$tx_speed,$rx_speed]"
-
+    
     # Update old values
     rx_old=$rx_new
     tx_old=$tx_new
