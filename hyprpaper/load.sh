ce51ecff7a3036b7b536245271f0f960ee741993
AymanLyesri <lyesri99@gmail.com>
2023-12-21 10:30:18 +0100

    Update bar styling and preload wallpapers in hyprpaper
    Former-commit-id: 0a970b258fc39f867e821bed339c4272c7dfb7a0
    Former-commit-id: 986fc39a8081ab83d0346796b083a3937abb7012

--- hyprpaper/load.sh
+++ hyprpaper/load.sh
@@ -9,16 +9,15 @@ hyprpaper_conf=$hyprDir/hyprpaper.conf           # hyprpaper config
 
 >$hyprpaper_conf # clear config
 
-echo "splash = true" >>$hyprpaper_conf # set splash screen
+echo "splash = true" >>$hyprpaper_conf          # set splash screen
+for wallpaper in $wallpapers; do                # loop through wallpapers
+    echo "preload=$wallpaper" >>$hyprpaper_conf # preload wallpaper
+done
 
 #################################################
 
 hyprpaper &
 
 sleep 1 # wait for hyprpaper to die
-
-for wallpaper in $wallpapers; do           # loop through wallpapers
-    hyprctl hyprpaper preload "$wallpaper" # preload wallpaper
-done
 
 source $hyprDir/hyprpaper/auto.sh # start auto wallpaper script
