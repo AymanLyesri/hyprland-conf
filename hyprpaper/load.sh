f6e40b19c2d7bd9be9af2f42c756aa010e1eb5e9
Ayman Lyesri <lyesri99@gmail.com>
2023-12-21 14:13:52 +0100

    fixed hyprpaper bugs
    Former-commit-id: a554e2cd21ae666a26abf0fffbc0f8759651dd3b
    Former-commit-id: abbbb8b1f2e6e1a59fb9ff16e78a399021a5d3f6

--- hyprpaper/load.sh
+++ hyprpaper/load.sh
@@ -5,19 +5,12 @@ defaults=$hyprDir/hyprpaper/config/defaults.conf # config file
 wallpapers=$(awk -F'=' '{print $2}' $defaults)   # get wallpapers
 hyprpaper_conf=$hyprDir/hyprpaper.conf           # hyprpaper config
 
-#############################################
+#################################################
 
->$hyprpaper_conf # clear config
-
-echo "splash = true" >>$hyprpaper_conf          # set splash screen
-for wallpaper in $wallpapers; do                # loop through wallpapers
-    echo "preload=$wallpaper" >>$hyprpaper_conf # preload wallpaper
+for wallpaper in $wallpapers; do           # loop through wallpapers
+    hyprctl hyprpaper preload "$wallpaper" # preload wallpaper
 done
 
 #################################################
 
-hyprpaper &
-
-sleep 1 # wait for hyprpaper to die
-
-source $hyprDir/hyprpaper/auto.sh # start auto wallpaper script
+$hyprDir/hyprpaper/auto.sh & # start auto wallpaper script
--- hyprpaper/reload.sh
+++ hyprpaper/reload.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
+hyprDir=$HOME/.config/hypr # hypr directory
 
-current_script=$(basename "$0")          # Get the current script's filename
-other_pids=$(pgrep -f "$current_script") # Get the PIDs of all other scripts with the same name
+# killall hyprpaper
+hyprctl hyprpaper unload all
+killall auto.sh
 
-for pid in $other_pids; do
-    if [ "$pid" != "$$" ]; then     # $$ represents the PID of the current script
-        echo "Killing process $pid" # This is just for debugging
-        kill "$pid"                 # Kill the other script
-    fi
-done
-
-#############################################
-
-killall load.sh   # kill load.sh
-killall hyprpaper # kill hyprpaper
-
-source $HOME/.config/hypr/hyprpaper/load.sh & # start load.sh
+$hyprDir/hyprpaper/load.sh
