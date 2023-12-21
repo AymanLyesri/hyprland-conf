0a970b258fc39f867e821bed339c4272c7dfb7a0
AymanLyesri <lyesri99@gmail.com>
2023-12-21 10:30:18 +0100

    Update bar styling and preload wallpapers in hyprpaper

--- hyprpaper/w.sh
+++ hyprpaper/w.sh
@@ -5,19 +5,16 @@ wallpaper=$1                                                   # wallpaper
 
 ##########################################
 
-# source $hyprdir/scripts/eww_reset & # reset eww
-
-##########################################
-
-# hyprctl hyprpaper preload "$wallpaper"                # preload wallpaper
 for monitor in $monitors; do                          # loop through monitors
     hyprctl hyprpaper wallpaper "$monitor,$wallpaper" # set wallpaper
 done
-# hyprctl hyprpaper unload all # unload wallpaper
 
 ##########################################
 
+sleep 5 # wait for wallpaper to load
+
 source $hyprdir/wal/pywal "$wallpaper"      # set wallpaper theme
 source $hyprdir/scripts/dynamic-border.sh & # set border theme
+# source $hyprdir/scripts/eww_reset &         # reset eww
 
 ##########################################
