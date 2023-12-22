5cfee04939df9594c88d4f1c0bb1bf1d5b1976c4
AymanLyesri <lyesri99@gmail.com>
2023-12-22 10:48:32 +0100

    fixed hyprland (maybe)
    Former-commit-id: 9c68e08ff96c5fca10c58651559e88cf21ad3c1e
    Former-commit-id: 35cdb22eff00ab6b56c1ddbf76e6a0cc8c22cac4

--- hyprpaper/auto.sh
+++ hyprpaper/auto.sh
@@ -7,19 +7,15 @@ previous_workspace_id=0
 change_wallpaper() {
     workspace_id=$(hyprctl monitors | grep active | awk '{print $3}') # get workspace id
     if [ "$workspace_id" != "$previous_workspace_id" ]; then          # if workspace id changed
-        wallpaper=$(grep "^w-$workspace_id=" $config | cut -d= -f2)   # get wallpaper from config
 
-        # sleep 0.75s
+        killall "w.sh"
 
-        #############################################
+        wallpaper=$(grep "^w-$workspace_id=" $config | cut -d= -f2) # get wallpaper from config
 
-        killall "w.sh" # kill previous wallpaper script
-
-        #############################################
-
-        $hyprDir/hyprpaper/w.sh "$wallpaper" & # set wallpaper
-
-        previous_workspace_id=$workspace_id # update previous workspace id
+        if [ "$wallpaper" != "" ]; then            # kill previous wallpaper script
+            $hyprDir/hyprpaper/w.sh "$wallpaper" & # set wallpaper
+            previous_workspace_id=$workspace_id    # update previous workspace id
+        fi
     fi
 }
 
