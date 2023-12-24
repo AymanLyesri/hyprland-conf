81476e1092133af3a379439234aed49c4341b046
Ayman Lyesri <lyesri99@gmail.com>
2023-12-24 22:01:24 +0100

    colorful bar :)
    Former-commit-id: 4c0174915d87f2d9022edd627ad4e7a6b58d4479
    Former-commit-id: 44f4b8434f541ac0b6090d435cb4062451f99f2b

--- eww/bar/scripts/window.sh
+++ eww/bar/scripts/window.sh
@@ -9,35 +9,43 @@ while true; do
 
     case $win_active in
     "Code")
-      style="background-color: rgb(100,100,200); color: rgb(255,255,255)"
+      background_image="100,100,200"
+      color="rgb(255,255,255)"
       ;;
     "kitty")
-      style="background-color: rgb(240,75,75); color: rgb(255,255,255)"
+      background_image="240,75,75"
+      color="rgb(255,255,255)"
       ;;
     "Spotify")
-      style="background-color: rgb(100,200,100); color: rgb(0,0,0)"
+      background_image="100,200,100"
+      color="rgb(0,0,0)"
       ;;
     "firefox")
-      style="background-color: rgb(240,155,100); color: rgb(255,255,255)"
+      background_image="240,155,100"
+      color="rgb(255,255,255)"
       ;;
     "Rofi")
-      style="background-color: rgb(255,255,255); color: rgb(0,0,0)"
+      background_image="255,255,255"
+      color="rgb(0,0,0)"
       ;;
     "Rofi")
-      style="background-color: rgb(255,255,255); color: rgb(0,0,0)"
+      background_image="255,255,255"
+      color="rgb(0,0,0)"
       ;;
     "dotnet")
-      style="background-color: rgb(255,0,200); color: rgb(255,255,255)"
+      background_image="255,0,200"
+      color="rgb(255,255,255)"
       ;;
     "steam")
-      style="background-color: rgb(0,0,155); color: rgb(255,255,255)"
+      background_image="0,0,155"
+      color="rgb(255,255,255)"
       ;;
     *)
       echo ""
       ;;
     esac
 
-    style+="; min-width:$(echo $win_active | wc -c | awk '{print $1 * 10}')px;"
+    style="background-image: linear-gradient(to bottom right, rgba($background_image, 0.5), rgba($background_image, 1), rgba($background_image, 0.5)); color:$color; min-width:$(echo $win_active | wc -c | awk '{print $1 * 10}')px;"
     echo "[\"$win_active\",\"$style\"]"
   else
     echo "[]"
