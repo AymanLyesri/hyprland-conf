0870817a4855eea40cecef6604025b8c54fd2390
AymanLyesri <lyesri99@gmail.com>
2023-12-26 12:32:57 +0100

    Update daily driver config with new features and improvements

--- eww/bar/scripts/bandwidth.sh
+++ eww/bar/scripts/bandwidth.sh
@@ -3,11 +3,13 @@
 # This script is used to show the bandwidth usage of a given interface
 
 # Get the network interface associated with Ethernet (enp)
-enp_interface=$(ip -o link show up | grep 'state UP' | awk '{print $2}' | sed 's/://')
+interface=$(ip -o link show up | grep 'state UP' | awk '{print $2}' | sed 's/://' | head -n 1)
+
+echo ${interface[0]}
 
 # Function to extract bytes from ip command output
 get_bytes() {
-    ip -s link show "$enp_interface" | awk '/'"$1"':/{getline; print $1}'
+    ip -s link show ${interface[0]} | awk '/'"$1"':/{getline; print $1}'
 }
 
 # Get initial values
@@ -19,14 +21,14 @@ while true; do
     # Get new values
     rx_new=$(get_bytes "RX")
     tx_new=$(get_bytes "TX")
-    
+
     # Calculate speed
     rx_speed=$((($rx_new - $rx_old) / 1024)) # KB per second with two decimal places
     tx_speed=$((($tx_new - $tx_old) / 1024)) # KB per second with two decimal places
-    
+
     # Display results
     echo "[$tx_speed,$rx_speed]"
-    
+
     # Update old values
     rx_old=$rx_new
     tx_old=$tx_new
