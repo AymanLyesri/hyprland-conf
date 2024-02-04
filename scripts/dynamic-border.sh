c2cf3e2bdf9c858c77d54af2eaf7024c87ad8934
Ayman Lyesri <lyesri99@gmail.com>
2024-02-04 14:25:52 +0100

    Update configurations and added custom things
    Former-commit-id: b5005d2a6cf80a45170f3739df4a03aa482ecffa
    Former-commit-id: 646e72e5f635b1e08e0f7ead478b8588d830e032

--- scripts/dynamic-border.sh
+++ scripts/dynamic-border.sh
@@ -8,26 +8,11 @@ dunst_file="$hypr_dir/notifications/dunstrc"                # Dunst config file
 # Extract hex color codes from CSS file and store them in an array
 colors=($(grep -o '#[0-9A-Fa-f]\{6\}' $css_file | sed 's/^#//'))
 
-# Define the new string
-new_colors="rgb(${colors[4]}) rgb(${colors[0]}) rgb(${colors[0]}) rgb(${colors[4]}) 270deg"
+for color in "${colors[@]}"; do
+    echo "$color"
+done
 
-# Use sed to replace the entire string after '=' with the new string
-# sed -i "s/col.active_border = .*/col.active_border = $new_colors/" $hyprland_border_file
+# Define the new string
+new_colors="rgb(${colors[3]}) rgb(${colors[4]}) rgb(${colors[5]}) rgb(${colors[6]}) 270deg"
 
 echo -e "general { \n\tcol.active_border = $new_colors \n}" >$hyprland_border_file
-
-# Use sed to replace the entire string after '=' with the new string
-# sed -i "s/frame_color = .*/frame_color = \"\#${colors[4]}\"/" $dunst_file # Use sed to replace the entire string after '=' with the new string
-# killall dunst                                                             # Restart dunst to apply changes
-
-# Assuming ${colors[4]} holds the desired color value
-# color_value="#${colors[4]}"
-
-# # Creating a temporary file
-# temp_file=$(mktemp) || exit 1
-
-# # Using sed to replace the pattern and redirecting output to the temporary file
-# sed "s/frame_color = .*/frame_color = \"$color_value\"/" "$dunst_file" >"$temp_file"
-
-# # Replace the original file with the temporary file
-# mv "$temp_file" "$dunst_file"
