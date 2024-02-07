8052681923ff2a58b6f5ba54dbaec3659768d831
Ayman Lyesri <lyesri99@gmail.com>
2024-02-07 22:09:10 +0100

    configuring nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,12 @@
-local packer = require('packer').startup(function()
+local neotree = require('plugins.neotree')
+
+require('packer').startup(function()
     use { 'wbthomason/packer.nvim', config = {
         -- Add auto-update settings
         auto_update = true
-    },
+        },
     }
+    use {neotree}
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
@@ -16,16 +19,6 @@ local packer = require('packer').startup(function()
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end,
-    }
-    use {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        }
     }
     use {
         'hrsh7th/nvim-cmp',
@@ -119,6 +112,10 @@ vim.g.neovide_padding_top = 0
 vim.g.neovide_padding_bottom = 0
 vim.g.neovide_padding_right = 0
 vim.g.neovide_padding_left = 0
+
+-- Automatically update Packer plugins when Neovim starts
+vim.cmd [[autocmd VimEnter * PackerSync]]
+
 
 -- vim.g.neovide_transparency = 0.69
 -- vim.g.transparency = 0.69
