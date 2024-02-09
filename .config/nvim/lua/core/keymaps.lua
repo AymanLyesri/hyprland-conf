cfa73eb202527a16d5eee4ef79bff115cf5e0576
Ayman Lyesri <lyesri99@gmail.com>
2024-02-09 11:37:39 +0100

    small tweaks

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -6,7 +6,16 @@ vim.api.nvim_set_keymap('n', 'k', 'l', { noremap = true })
 vim.api.nvim_set_keymap('n', 'h', 'j', { noremap = true })
 vim.api.nvim_set_keymap('n', 'l', 'k', { noremap = true })
 
+-- Rebind directional window-switching keys for Dvorak layout
+vim.api.nvim_set_keymap('n', '<C-w>n', '<C-w>h', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-w>e', '<C-w>j', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-w>i', '<C-w>k', { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-w>o', '<C-w>l', { noremap = true })
+
+-- Reload nvim configuration
+vim.api.nvim_set_keymap('n', '<Leader>r', ':source $MYVIMRC<CR>', { noremap = true, silent = true })
+
 
 -- terminal mappings
-vim.api.nvim_set_keymap('n', '<Leader>t', ':ToggleTerm size=30 direction=vertical name=desktop<CR>',
+vim.api.nvim_set_keymap('n', '<Leader>t', ':ToggleTerm size=50 direction=float name=term<CR>',
     { noremap = true, silent = true })
