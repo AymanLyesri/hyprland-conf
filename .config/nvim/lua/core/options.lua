0399fe8ea757ddce8e8b81bedee32ff85b49064a
AymanLyesri <lyesri99@gmail.com>
2024-02-10 17:34:56 +0100

    nvim improvement + small tweaks

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -10,12 +10,24 @@ vim.opt.shiftwidth = 4
 -- Use spaces instead of tabs for indentation
 vim.opt.expandtab = true
 
+-- Set italics for certain keywords
+--vim.highlight.create('ColorColumn', { ctermbg = 0, guibg = lightgrey }, false)
+-- Apply italic styling to comments
+vim.cmd [[highlight Comment cterm=italic gui=italic]]
+vim.cmd [[highlight Type cterm=italic gui=italic]]
+vim.cmd [[highlight Function cterm=italic gui=italic]]
+vim.cmd [[highlight Keyword cterm=italic gui=italic]]
+vim.cmd [[highlight String cterm=italic gui=italic]]
+vim.cmd [[highlight Number cterm=italic gui=italic]]
+
 
 -- Set encoding to UTF-8
 vim.opt.encoding = "UTF-8"
 
 -- Neovide settings
-vim.g.neovide_padding_top = 0
-vim.g.neovide_padding_bottom = 0
-vim.g.neovide_padding_right = 0
-vim.g.neovide_padding_left = 0
+--vim.g.neovide_padding_top = 0
+--vim.g.neovide_padding_bottom = 0
+--vim.g.neovide_padding_right = 0
+--vim.g.neovide_padding_left = 0
+vim.g.neovide_transparency = 0.69
+vim.o.guifont = "Hack Nerd Font Mono:h14"
--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -1,53 +1,67 @@
 local plugins = {
-	"nvim-tree/nvim-web-devicons",
-	'andweeb/presence.nvim',
-	'nvim-lualine/lualine.nvim',
-	'AlphaTechnolog/pywal.nvim',
-	{
-		'nvim-telescope/telescope.nvim',
-		tag = '0.1.5',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
+    "nvim-tree/nvim-web-devicons",
+    'andweeb/presence.nvim',
+    --ui
+    'nvim-lualine/lualine.nvim',
+    'navarasu/onedark.nvim',
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.5',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- add any options here
+        },
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
+    },
+    --highlight
+    'nvim-treesitter/nvim-treesitter',
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {} },
 
-	--highlight
-	'nvim-treesitter/nvim-treesitter',
-	{ "lukas-reineke/indent-blankline.nvim", main = "ibl",  opts = {} },
+    --lsp diagnostics
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
 
-	--lsp diagnostics
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
-
-	--completion
-	"lukas-reineke/lsp-format.nvim",
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	"L3MON4D3/LuaSnip",
-	"saadparwaiz1/cmp_luasnip",
-	"rafamadriz/friendly-snippets",
-	"github/copilot.vim",
-	"m4xshen/autoclose.nvim",
-
-	{ 'akinsho/toggleterm.nvim',             version = "*", config = true },
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		init = function()
-			vim.o.timeout = true
-			vim.o.timeoutlen = 300
-		end,
-		opts = {
-		}
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		}
-	},
+    --completion
+    "lukas-reineke/lsp-format.nvim",
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+    "saadparwaiz1/cmp_luasnip",
+    "rafamadriz/friendly-snippets",
+    "github/copilot.vim",
+    "m4xshen/autoclose.nvim",
+    { 'akinsho/toggleterm.nvim',             version = "*", config = true },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+        }
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    },
 }
 
 require("lazy").setup(plugins)
