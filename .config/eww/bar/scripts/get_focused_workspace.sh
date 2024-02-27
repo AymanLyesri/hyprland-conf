fbfbc90403172004721f95cee039eb0afb7a9d2e
AymanLyesri <lyesri99@gmail.com>
2024-02-27 23:01:39 +0100

    minor tweaks

--- .config/eww/bar/scripts/get_focused_workspace.sh
+++ .config/eww/bar/scripts/get_focused_workspace.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-get_focused(){
+get_focused() {
     echo $(hyprctl monitors | grep active | awk '{print $3}')
 }
-
 
 get_focused
 socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | while read -r line; do
