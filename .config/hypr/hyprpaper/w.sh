6b198ac3741b95b378ea1fb430e2fb5b763719a5
AymanLyesri <lyesri99@gmail.com>
2024-03-09 19:15:13 +0100

    optimized screenshot all script
    Former-commit-id: fc1144a53276868a025eddb460b6c6f85f283986
    Former-commit-id: 0f3d23aeda9b47b447337023cfc248c9f6fdc96b

--- .config/hypr/hyprpaper/w.sh
+++ .config/hypr/hyprpaper/w.sh
@@ -18,7 +18,6 @@ done
 sleep 0.5s
 $hyprdir/wal/pywal "$wallpaper" # set wallpaper theme # wait for wallpaper to load
 
-sleep 1s
 $hyprdir/scripts/dynamic-border.sh # set border theme
 # sleep 10s && echo ' ' >$hyprdir/eww/bar/scss/soft_reset.scss # reset eww bar
 
