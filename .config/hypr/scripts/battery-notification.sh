ae94389132c45185cbf66656567e4b967f3b3c89
Ayman Lyesri <lyesri99@gmail.com>
2024-04-15 17:26:13 +0100

    added battery notif, kitty config
    Former-commit-id: 63fe5c8ae739e0901be9d1d08ef7b7aeeeb2651b
    Former-commit-id: 8ff339d32adb08a515eee2272bfee6225dfb5d72

---
+++ .config/hypr/scripts/battery-notification.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+while true; do
+    # Get battery percentage
+    battery_percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -E "percentage" | awk '{print $2}')
+
+    # Remove the % sign
+    battery_percentage=${battery_percentage%\%}
+
+    # Check if battery level is less than or equal to 25%
+    if [ "$battery_percentage" -le 25 ]; then
+        # Send alert notification every 1 second until charging
+        while true; do
+            # Send alert notification
+            notify-send -u critical "Battery Low!" "Battery level is at $battery_percentage%. Please plug in your charger."
+
+            # Get charging status
+            charging_status=$(upower -i /org/freedesktop/UPower/devices/battery_BAT1 | grep -E "state" | awk '{print $2}')
+
+            # Check if charging
+            if [ "$charging_status" = "charging" ]; then
+                break # Exit the inner loop if charging
+            fi
+
+            # Sleep for 1 second before sending the next notification
+            sleep 10
+        done
+    fi
+
+    # Sleep for 5 minutes before checking battery level again
+    sleep 600 # 300 seconds = 5 minutes
+done
