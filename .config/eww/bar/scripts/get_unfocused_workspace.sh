305377b4b64d5d92aa7f100b7d7451cd86561c49
AymanLyesri <lyesri99@gmail.com>
2024-06-15 13:18:52 +0100

    fixed bugs

--- .config/eww/bar/scripts/get_unfocused_workspace.sh
+++ .config/eww/bar/scripts/get_unfocused_workspace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-socat -u UNIX-CONNECT:/tmp/hypr/"$HYPRLAND_INSTANCE_SIGNATURE"/.socket2.sock - | while read -r line; do
+socat -U - UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r line; do
     sleep 0.5
     echo $(hyprctl monitors | grep active | awk '{print $3}')
 done
