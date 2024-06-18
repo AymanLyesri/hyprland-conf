1d267ae8e050d19c712ac74bc84ea0a164f4d6e4
AymanLyesri <lyesri99@gmail.com>
2024-06-18 12:31:32 +0100

    system theme architecture overhaul

---
+++ .config/hypr/theme/scripts/system-theme.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+hyprDir=$HOME/.config/hypr
+
+theme_conf=$hyprDir/theme/theme.conf
+
+#check for $1
+
+if [ -z $1 ]; then
+    echo "No argument provided: get or switch"
+    exit 1
+fi
+
+current_theme=$(grep '^theme=' $theme_conf | cut -d'=' -f2)
+
+if [ $1 == "get" ]; then
+    echo $current_theme
+    exit 0
+elif
+    [ $1 == "switch" ]
+then
+    if [ "$current_theme" == "dark" ]; then
+        sed -i 's/^theme=dark$/theme=light/' "$theme_conf"
+    elif [ "$current_theme" == "light" ]; then
+        sed -i 's/^theme=light$/theme=dark/' "$theme_conf"
+    else
+        echo "Current theme is neither 'dark' nor 'light'."
+    fi
+    exit 0
+else
+    echo "Invalid argument"
+    exit 1
+fi
