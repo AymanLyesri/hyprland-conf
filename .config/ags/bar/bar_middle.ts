3ce3a022847c1002c39d2b663321718d1d12e65b
AymanLyesri <lyesri99@gmail.com>
2024-06-22 21:00:20 +0100

    media player changes in ags
    Former-commit-id: c059a3239030299318b1faef01fd594c613691bd
    Former-commit-id: c477873070201b9dc660755e92736b9bc44de932

--- .config/ags/bar/bar_middle.ts
+++ .config/ags/bar/bar_middle.ts
@@ -22,30 +22,73 @@ function Notification()
 
 function Media()
 {
-    const label = Widget.Label({
-        label: Utils.watch("", mpris, "changed", () =>
+    function getComplementaryColor(hex: string): string
+    {
+        // Remove the hash at the start if it's there
+        hex = hex.replace(/^#/, '');
+
+        // Parse the r, g, b values
+        let r = parseInt(hex.substring(0, 2), 16);
+        let g = parseInt(hex.substring(2, 4), 16);
+        let b = parseInt(hex.substring(4, 6), 16);
+
+        // Get the complementary color
+        r = 255 - r;
+        g = 255 - g;
+        b = 255 - b;
+
+        // Convert back to a hex string and return
+        return '#' + [r, g, b].map(x =>
+        {
+            const hex = x.toString(16);
+            return hex.length === 1 ? '0' + hex : hex;
+        }).join('');
+    }
+
+    function playerToBackground(name)
+    {
+        let color = "";
+        switch (name) {
+            case "spotify":
+                color = "#1ED760";
+                break;
+            case "VLC media player":
+                color = "#FF5722";
+                break;
+            case "YouTube":
+                color = "#FF0000";
+                break;
+            case "Brave Browser":
+                color = "#FF5722";
+                break;
+            case "Audacious":
+                color = "#FF5722";
+                break;
+            case "Rhythmbox":
+                color = "#FF5722";
+                break;
+            case "Chromium":
+                color = "#FF5722";
+                break;
+            case "Firefox":
+                color = "#FF5722";
+                break;
+            default:
+                color = "#FF5722";
+                break;
+        }
+        return color;
+    }
+    const label: string = Widget.Label({
+        label: Utils.watch(`${mpris.players[0].track_artists.join(", ")} - ${mpris.players[0].track_title}`, mpris, "changed", () =>
         {
             if (mpris.players[0]) {
                 const { track_artists, track_title } = mpris.players[0];
                 return `${track_artists.join(", ")} - ${track_title}`;
             } else {
-                return "Nothing is playing";
+                return 'Nothing is playing';
             }
         }),
-    })
-
-    /** @param {import('types/service/mpris').MprisPlayer} player */
-    const Player = player => Widget.Button({
-        onClicked: () => player.playPause(),
-        child: Widget.Label().hook(player, label =>
-        {
-            const { track_artists, track_title } = player;
-            label.label = `${track_artists.join(', ')} - ${track_title}`;
-        }),
-    })
-
-    const players = Widget.Box({
-        children: mpris.bind('players').as(p => p.map(Player))
     })
 
 
@@ -56,27 +99,46 @@ function Media()
         child: label,
     })
 
-    const revealer = Widget.Revealer({
-        revealChild: false,
-        transitionDuration: 1000,
-        transition: 'slide_right',
-        child: players,
-        // setup: self => self.poll(2000, () =>
-        // {
-        //     self.reveal_child = !self.reveal_child;
-        // }),
-    });
+    let player = mpris.getPlayer("")
 
-    const trigger = Widget.Button({
-        on_hover: () => revealer.reveal_child = true,
-        on_hover_lost: () => setTimeout(() => revealer.reveal_child = false, 5000),
-        child: Widget.Icon({ icon: "media-playback-start-symbolic" }),
-        class_name: "media-trigger",
-    });
+    const progress = Widget.CircularProgress({
+        rounded: false,
+        inverted: false,
+        startAt: 0.75,
+        // value: mpris.getPlayer("").bind('').as(p => p / 100),
+        child: Widget.Icon({
+            icon: "media-playback-start-symbolic",
+        }),
+        setup: self =>
+        {
+            function update()
+            {
+                const value = player?.position / player?.length
+                self.value = value > 0 ? value : 0
+            }
+            self.hook(player, update)
+            // self.hook(player, update, "position")
+            self.poll(1000, update)
+        },
+    })
+
+    // const revealer = Widget.Revealer({
+    //     revealChild: false,
+    //     transitionDuration: 1000,
+    //     transition: 'slide_right',
+    //     child: media,
+    //     // setup: self => self.poll(2000, () =>
+    //     // {
+    //     //     self.reveal_child = !self.reveal_child;
+    //     // }),
+    // });
 
     return Widget.Box({
         class_name: "media",
-        children: [trigger, revealer]
+        children: [media],
+        css: `background: ${playerToBackground(mpris.getPlayer("")?.name)};
+        color: ${getComplementaryColor(playerToBackground(mpris.getPlayer("")?.name))}
+        `,
     })
 }
 
