29bc517b5189bb774cb4641d880d60bd210c6f8c
AymanLyesri <lyesri99@gmail.com>
2024-09-26 23:26:48 +0100

    smoother animations, tweaks

--- .config/hypr/hyprpaper/w.sh
+++ .config/hypr/hyprpaper/w.sh
@@ -12,5 +12,7 @@ for monitor in "${monitors[@]}"; do
     hyprctl hyprpaper wallpaper "$monitor,$wallpaper"
 done
 
+sleep 1 # Wait for wallpaper to be set (remove stutering)
+
 # Set wallpaper theme
 $hyprdir/theme/scripts/wal-theme.sh "$wallpaper"
