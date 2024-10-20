a47d936e6125a1cc3f1374e9d30b6cf41d12357a
Ayman Lyesri <lyesri99@gmail.com>
2024-10-20 21:02:02 +0100

    custom wallpapers could be easily added + tweaks

--- .config/hypr/hyprpaper/load.sh
+++ .config/hypr/hyprpaper/load.sh
@@ -5,9 +5,18 @@ defaults=$hyprDir/hyprpaper/config/defaults.conf # config file
 wallpapers=$(awk -F'=' '{print $2}' $defaults)   # get wallpapers
 hyprpaper_conf=$hyprDir/hyprpaper.conf           # hyprpaper config
 
+default_wallpapers=$HOME/.config/wallpapers/default # default wallpapers directory
+custom_wallpapers=$HOME/.config/wallpapers/custom   # custom wallpapers directory
+all_wallpapers=$HOME/.config/wallpapers/all         # all wallpapers directory
+
 #################################################
 
-for wallpaper in $wallpapers; do           # loop through wallpapers
+rm -rf $all_wallpapers && mkdir -p $all_wallpapers && cp -r $default_wallpapers/* $custom_wallpapers/* $all_wallpapers # copy default and custom wallpapers to all wallpapers directory
+
+#################################################
+
+# loop through wallpapers
+for wallpaper in $wallpapers; do
     hyprctl hyprpaper preload "$wallpaper" # preload wallpaper
 done
 
