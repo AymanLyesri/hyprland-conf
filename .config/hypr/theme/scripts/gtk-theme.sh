56d2fb69c007da3ce067c8ca7f208699c6528f24
AymanLyesri <lyesri99@gmail.com>
2024-10-25 12:08:11 +0100

    fixed cursor theme on bootup

--- .config/hypr/theme/scripts/gtk-theme.sh
+++ .config/hypr/theme/scripts/gtk-theme.sh
@@ -4,5 +4,3 @@ hyprDir=$HOME/.config/hypr
 current_theme=$(bash $hyprDir/theme/scripts/system-theme.sh get)
 
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-$current_theme
-
-gsettings set org.gnome.desktop.interface cursor-theme phinger-cursors-$current_theme
