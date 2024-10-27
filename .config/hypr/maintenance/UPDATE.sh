b3263f4d090768a0344b61770419a32a979cae00
Ayman Lyesri <lyesri99@gmail.com>
2024-10-27 11:44:26 +0100

    workspace config is now unique to user

--- .config/hypr/maintenance/UPDATE.sh
+++ .config/hypr/maintenance/UPDATE.sh
@@ -15,9 +15,19 @@ git checkout $BRANCH
 git fetch origin $BRANCH
 git reset --hard origin/$BRANCH
 
-$HOME/.config/hypr/maintenance/DEFAULTS.sh
-
 aur_helpers=("yay" "paru")
-aur_helper=$(echo "${aur_helpers[@]}" | tr ' ' '\n' | fzf --height "40%")
 
-continue_prompt "Do you want to install necessary packages? (using $aur_helper)" "$HOME/.config/hypr/pacman/install-pkgs.sh $aur_helper"
+for helper in "${aur_helpers[@]}"; do
+    if command -v "$helper" &>/dev/null; then
+        aur_helper="$helper"
+        break
+    fi
+done
+
+if [[ -z "$aur_helper" ]]; then
+    echo "No AUR helper (yay or paru) is installed."
+else
+    continue_prompt "Do you want to install necessary packages? (using $aur_helper)" "$HOME/.config/hypr/pacman/install-pkgs.sh $aur_helper"
+fi
+
+continue_prompt "Do you want to set up default custom files" "$HOME/.config/hypr/maintenance/DEFAULTS.sh"
