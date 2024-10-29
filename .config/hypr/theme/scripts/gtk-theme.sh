045ccf6861afa7c77a4b3bfcae1c3a51ceb431de
Ayman Lyesri <lyesri99@gmail.com>
2024-10-29 22:36:53 +0100

    using WhiteSur gtk theme

--- .config/hypr/theme/scripts/gtk-theme.sh
+++ .config/hypr/theme/scripts/gtk-theme.sh
@@ -3,4 +3,6 @@
 hyprDir=$HOME/.config/hypr
 current_theme=$(bash $hyprDir/theme/scripts/system-theme.sh get)
 
-gsettings set org.gnome.desktop.interface gtk-theme Adwaita-$current_theme
+gsettings set org.gnome.desktop.interface gtk-theme WhiteSur-${current_theme^}
+
+echo "GTK theme set to WhiteSur-$current_theme"
