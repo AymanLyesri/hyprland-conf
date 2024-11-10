244b58d1b4a514a2fadd6872351f2068188ea05f
Ayman Lyesri <lyesri99@gmail.com>
2024-11-10 19:45:33 +0100

    tweaks

--- .config/ags/utils/scss.ts
+++ .config/ags/utils/scss.ts
@@ -16,11 +16,14 @@ export function refreshCss()
     // main scss file
     const scss = `${App.configDir}/scss/style.scss`
 
-    const response = Utils.exec(`bash -c "echo '$OPACITY: ${globalOpacity.value};' | cat - ${defaultColors} ${walColors} ${scss} > ${tmpScss} | sassc ${tmpScss} ${tmpCss} -I ${scss_dir} -I ${cache_dir}"`)
-    print(response)
-    if (response != "") Utils.notify(response)
-    App.resetCss()
-    App.applyCss(tmpCss)
+    Utils.execAsync(`bash -c "echo '$OPACITY: ${globalOpacity.value};' | cat - ${defaultColors} ${walColors} ${scss} > ${tmpScss} | sassc ${tmpScss} ${tmpCss} -I ${scss_dir}"`)
+        .then((response) => { if (response != "") Utils.notify(response) })
+        .finally(() =>
+        {
+            App.resetCss()
+            App.applyCss(tmpCss)
+        })
+        .catch(err => Utils.notify(err))
 }
 
 Utils.monitorFile(
