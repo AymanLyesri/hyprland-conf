c71d5cf15510abad52235eecd5f72701db852cac
Ayman Lyesri <lyesri99@gmail.com>
2025-03-08 14:46:42 +0000

    fixed translation, removed some wallpapers

--- .config/ags/scripts/translate.sh
+++ .config/ags/scripts/translate.sh
@@ -2,15 +2,18 @@
 
 # Translate a text to English
 text=$1
-launguage=$2
+language=$2
 
-if [ -z "$launguage" ]; then
-    launguage="en"
+if [ -z "$language" ]; then
+    language="en"
 fi
 
-result=$(trans -brief "$text" -t $launguage)
+result=$(trans -brief "$text" -t "$language")
+
+# Escape single quotes in the result
+escaped_result=$(echo "$result" | sed "s/'/'\\\\''/g")
 
 app_name=$result
-app_exec="wl-copy '$result'"
+app_exec="wl-copy '$escaped_result'"
 
 echo "[{\"app_name\": \"$app_name\", \"app_exec\": \"$app_exec\"}]"
