4e3cc7ed1c1bf0c2c5b4fd015af0e77cde9731a6
Ayman Lyesri <lyesri99@gmail.com>
2025-03-14 19:28:14 +0000

    full upgrade before installing the config

--- .config/hypr/pacman/install-pkgs.sh
+++ .config/hypr/pacman/install-pkgs.sh
@@ -9,4 +9,4 @@ else
     package_manager=$1
 fi
 
-sudo grep -vE '^\s*#|^\s*$' $HOME/.config/hypr/pacman/pkglist.txt | $package_manager -Sy --needed -
+sudo grep -vE '^\s*#|^\s*$' $HOME/.config/hypr/pacman/pkglist.txt | $package_manager -Syu --needed -
